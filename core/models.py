9565d0ffa5f0681f78e57efeb8ba865b84fc7532
Fernando Neto <devfernandonetoo@gmail.com>
2026-01-21 19:02:46 -0300

    Configuração do banco de dados para uso do PostgreSQL, criação do MODEL Filmes no app CORE, execução de makemigrations e migrate para funcionar se o projeto está rodando sem quebrar e, por fim, realizei a inserção das variáveis sensíveis no arquivo .env para que não sejam enviadas para o GitHub.

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Filme(models.Model):
+    titulo = models.CharField(max_length=200)
+    descricao = models.TextField()
+    ano_lancamento = models.IntegerField()
+    
+    def __str__(self):
+        return self.titulo
--- setup/settings.py
+++ setup/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -72,12 +74,22 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+environ.Env.read_env(BASE_DIR / '.env')
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
     }
 }
+
 
 
 # Password validation
