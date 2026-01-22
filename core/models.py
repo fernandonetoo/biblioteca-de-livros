bf99a734bcd5c534b9db4596cd57a9939cd2bebf
Fernando Neto <devfernandonetoo@gmail.com>
2026-01-22 18:53:15 -0300

    Fiz a alteração do Model Filme para receber imagens, eme seguida fiz a configuração do caminho para que o Django entenda onde estão e onde serão salvas as imagens. Instalei a biblioteca Pillow para que o Django consiga trabalhar com imagens. Por fim, fiz a atualização do requirements.txt.

--- core/models.py
+++ core/models.py
@@ -6,6 +6,8 @@ class Filme(models.Model):
     titulo = models.CharField(max_length=200)
     descricao = models.TextField()
     ano_lancamento = models.IntegerField()
+    capa = models.ImageField(upload_to='filmes/', null=True, blank=True)
+
     
     def __str__(self):
         return self.titulo
